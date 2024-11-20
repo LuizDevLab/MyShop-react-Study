c856b967a61a53001689df3d8f846cf459604976
Luiz Henrique <luizgthub@gmail.com>
2024-11-20 19:32:50 -0300

    add redux & reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
+import { Provider } from "react-redux";
 import { Header } from "./components/Header/Header_temp";
 import { ProductsList } from "./components/ProductsList/ProductsList";
 import { GlobalStyles } from "./styles/GlobalStyles";
+import { store } from "./Redux/store";
 
+// componente funcional do React
 function App() {
   return (
-    <>
+    <Provider store={store}>
       <Header />
       <ProductsList />
       <GlobalStyles/>
-    </>
+    </Provider>
   );
 }
 
--- src/components/Header/Header_temp.tsx
+++ src/components/Header/Header_temp.tsx
@@ -2,24 +2,50 @@ import { FiLogIn, FiLogOut, FiShoppingCart } from "react-icons/fi";
 import * as S from "./Styles";
 import { useState } from "react";
 import { Cart } from "../Cart/Cart";
+import { useDispatch, useSelector } from "react-redux";
+import { RootReducer } from "../../Redux/root-reducer";
 
 export const Header: React.FC = () => {
-  const [showCart, setShowCart] = useState(false)
-  const isLogged = true;
+  const { user } = useSelector(
+    (rootReducer: RootReducer) => rootReducer.userReducer
+  );
+
+  const dispatch = useDispatch();
+
+  const [showCart, setShowCart] = useState(false);
+  const isLogged = user !== null;
+
+  function handleUserAuth() {
+    if (user === null) {
+      //despachar a action de login
+      dispatch({
+        type: "user/login",
+        payload: {
+          name: "Luiz Henrique",
+          email: "luiz@email.com",
+        },
+      });
+    } else {
+      dispatch({
+        type: "user/logout",
+      });
+    }
+  }
+
 
   return (
     <S.StyledHeader>
       <S.Wrapper>
         <S.HeaderTitle>MyShop.</S.HeaderTitle>
         <S.ButtonsWrapper>
-          <S.AuthButton isLogged={isLogged}>
+          <S.AuthButton isLogged={isLogged} onClick={handleUserAuth}>
             {isLogged ? "Logout" : "Login"}
-            {isLogged ? <FiLogOut /> : <FiLogIn/>}
+            {isLogged ? <FiLogOut /> : <FiLogIn />}
           </S.AuthButton>
 
           <S.CartButton onClick={() => setShowCart(!showCart)}>
             Carrinho
-            <FiShoppingCart/>
+            <FiShoppingCart />
           </S.CartButton>
         </S.ButtonsWrapper>
       </S.Wrapper>
